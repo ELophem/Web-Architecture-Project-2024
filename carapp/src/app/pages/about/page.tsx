4d463d457ca9311ec78878b8439fab077f2b305c
ELophem <18072@ecam.be>
2024-03-20 23:53:41 +0100

    Added Routing, Pages, NavBar

--- carapp/src/app/pages/about/page.tsx
+++ carapp/src/app/pages/about/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1 className="text-4xl font-bold">Home</h1>
+    <h1 className="text-4xl font-bold">About</h1>
   </main>
+ 
   );
 }
--- carapp/src/app/pages/catalog/page.tsx
+++ carapp/src/app/pages/catalog/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Catalog() {
   return (
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1 className="text-4xl font-bold">Home</h1>
+    <h1 className="text-4xl font-bold">Catalog</h1>
   </main>
+ 
   );
 }
