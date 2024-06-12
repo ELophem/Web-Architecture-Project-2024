532f986a2755f7c600e88f36626eaaf0d3d5d91b
ELophem <18072@ecam.be>
2024-06-12 19:04:15 +0200

    QuickFix

--- carapp/src/app/catalog/page.tsx
+++ carapp/src/app/catalog/page.tsx
@@ -3,7 +3,7 @@ import CarList from '../components/CarList/CarList';
 
 export default function Catalog() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 pt-24">
       <h1 className="text-4xl font-bold">Catalog of Available Cars</h1>
       <CarList />
     </main>
